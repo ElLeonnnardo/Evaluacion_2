ff0901e24a7b737c7248fcaacf2469c150a3327c
Sebastian Chacon <Seb.chacon@duocuc.cl>
2023-11-15 21:01:35 -0300

    establecí el manejo de error 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -9,11 +9,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { DatosRegionalesService } from './services/location.service';
 import { DatosComunalesService } from './services/location.service';
+import { LoginPage } from './login/login.page';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginPage },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    RouterModule.forRoot([]),
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
@@ -22,7 +28,8 @@ import { DatosComunalesService } from './services/location.service';
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    DatosRegionalesService, DatosComunalesService, // Agrega el servicio aquí
+    DatosRegionalesService,
+    DatosComunalesService,
   ],
   bootstrap: [AppComponent],
 })
