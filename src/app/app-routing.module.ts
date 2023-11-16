42dbe2dd434b223f864395d9711e5d87e70964ea
Sebastian Chacon <Seb.chacon@duocuc.cl>
2023-11-15 22:28:04 -0300

    se integro la seguridad de ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LeerQrComponent } from './leer-qr/leer-qr.component';
+import { RegistroClaseComponent } from './registro-clase/registro-clase.component';
+import { PasswordResetPage } from './password-reset/password-reset.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule) },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'password-reset', component: PasswordResetPage },
+  { path: 'leer-qr', component: LeerQrComponent, canActivate: [AuthGuard] },
+  { path: 'registro-clase', component: RegistroClaseComponent, canActivate: [AuthGuard] },
   {
     path: 'leer-qr',
     loadChildren: () => import('./leer-qr/leer-qr.module').then( m => m.LeerQRPageModule)
@@ -17,8 +28,15 @@ const routes: Routes = [
     path: 'password-reset',
     loadChildren: () => import('./password-reset/password-reset.module').then( m => m.PasswordResetPageModule)
   },
-
-
+  {
+    path: 'ruta-protegida',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./ruta-protegida/ruta-protegida.module').then(m => m.RutaProtegidaPageModule),
+  },
+  {
+    path: 'ruta-protegida',
+    loadChildren: () => import('./ruta-protegida/ruta-protegida.module').then( m => m.RutaProtegidaPageModule)
+  },
 ];
 
 @NgModule({
